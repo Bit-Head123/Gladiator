9a589989944ddd83fa7ad3203715f4bb579748df
Bit-Head123 <bitheadlti@gmail.com>
2020-09-21 11:04:11 +0530

    account-summary commit by Abhinav

--- BoLTI/src/app/app.module.ts
+++ BoLTI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountSummaryComponent } from './account-summary/account-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSummaryComponent
   ],
   imports: [
     BrowserModule,
