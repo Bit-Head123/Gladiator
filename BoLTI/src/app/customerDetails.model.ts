7fd7731a5c6ffed8b2d0b6eb99d2ef0664c0c5b1
Bit-Head123 <mitshu1221@gmail.com>
2020-10-03 12:20:08 +0530

    models commit

--- BoLTI/src/app/customerDetails.model.ts
+++ BoLTI/src/app/customerDetails.model.ts
@@ -1,3 +1,12 @@
 export class CustomerDetail{
-
+   aadharNumber:number;
+   panNumber:String;
+   email:String;
+   mobileNumber:String;
+   age:number;
+   street:String;
+   city:String;
+   state:String;
+   pincode:number;
+   
 }
